caab6f1448a3df26a2938892fc9235cfd57b5d2d
othmane <othmane.alaouiekkaoui@gmail.com>
2023-03-21 14:51:26 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,17 @@
 void print_alphabet_x10(void)
 
 {
-	int i;
+	int i = 0;
 	char c;
 
-	for (i = 0; i < 10; i++)
+        while(i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
 	_putchar('\n');
+	i++;
         }
 }
 
