71ed051949a66aeb490919f8703463275cb4331a
othmane <othmane.alaouiekkaoui@gmail.com>
2023-03-22 07:38:18 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ */
+
+int main(void)
+{
+	int i, z = 0;
+
+	while(i < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	z += i;
+	}
+	i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
