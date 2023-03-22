c99bc3aeccdeba80e62c141f925e28ca0da3898c
othmane <othmane.alaouiekkaoui@gmail.com>
2023-03-22 06:53:04 -0700

    11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,32 +7,38 @@
  */
 
 void print_to_98(int n)
+
 {
-	if (n < 98)
-        {
-	while (n <= 98)
+	if (n <= 98)
+	{
+	for (; n <=98; n++)
+	{
+	if (n == 98)
 	{
 	printf("%d, ", n);
-	n++;
-	if (n == 98)
-	printf("%d", n);
-	}
 	printf("\n");
-	}
-	else if (n > 98)
-	{
-	while (n >= 98)
-	{
-	printf("%d, ", n);
-	n--;
-	if (n == 98)
-	printf("%d", n);
-	}
-	printf("\n");
+	break;
 	}
 	else
 	{
-	printf("%d",n);
+	printf("%d, ", n);
+	}
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
 	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
 	}
 }
