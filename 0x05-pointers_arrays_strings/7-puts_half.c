daf107cfeae96c1f43d6f2f4364e8d544444ea9b
othmane <othmane.alaouiekkaoui@gmail.com>
2023-03-27 19:10:17 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to check
+ * Return: Nothing!
+ */
+
+void puts_half(char *str)
+{
+	int i = 1, a;
+
+	while (str[i] != 0)
+		i++;
+	if (i % 2 == 0)
+	{
+		for (a = i / 2; a < i; a++)
+			_putchar(str[a]);
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = (i - 1) / 2; a <= i; a++)
+			_putchar(str[a]);
+		_putchar('\n');
+	}
+}
