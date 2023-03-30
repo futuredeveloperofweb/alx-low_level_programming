e3f6aecfc776a7720dba92987b928dd827c76f91
othmane <othmane.alaouiekkaoui@gmail.com>
2023-03-30 00:20:57 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,16 +10,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int destlen = 0, srclen = 0;
-	int i;
+	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-		destlen++;
-	for (i = 0; src[i] != '\0'; i++)
-		srclen++;
-	for (i = 0; i < n; i++)
+		continue;
+	for (j = 0; src[i] != '\0' && j < n; i++)
 	{
-		dest[destlen + i] = src[i];
+		dest[i + j] = src[j];
 	}
+	dest[i + j] = '\0';
 	return (dest);
 }
