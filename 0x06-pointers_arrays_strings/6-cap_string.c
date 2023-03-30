24dec8e900a9b72e4ee9b2efb0612447a7da0595
othmane <othmane.alaouiekkaoui@gmail.com>
2023-03-30 03:58:29 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @n: a pointer
+ * Return: char
+ */
+
+char *cap_string(char *n)
+{
+	int i;
+
+	i = 0;
+
+	while (n[i] != '\0')
+	{
+		while (n[i] >= 97 && n[i] <= 122)
+		{
+			if (n[i - 1] == ' ' ||
+			    n[i - 1] == '\t' ||
+			    n[i - 1] == '\n' ||
+			    n[i - 1] == ',' ||
+			    n[i - 1] == ';' ||
+			    n[i - 1] == '.' ||
+			    n[i - 1] == '!' ||
+			    n[i - 1] == '?' ||
+			    n[i - 1] == '"' ||
+			    n[i - 1] == '(' ||
+			    n[i - 1] == ')' ||
+			    n[i - 1] == '{' ||
+			    n[i - 1] == '}' ||
+			    i == 0)
+				n[i] -= 32;
+		i++;
+		}
+	}
+	return (n);
+}
