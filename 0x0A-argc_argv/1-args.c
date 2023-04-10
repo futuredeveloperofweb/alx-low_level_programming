67d4b5e0ee881a2ed996216df55e4b19e79e69b4
othmane <othmane.alaouiekkaoui@gmail.com>
2023-04-10 05:23:44 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the number of arguments
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
