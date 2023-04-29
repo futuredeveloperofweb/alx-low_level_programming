743d1918f8313c765a726414e31b2da9bdf9911a
othmane <othmane.alaouiekkaoui@gmail.com>
2023-04-29 13:27:34 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,30 +15,35 @@ size_t looped_listint_len(const listint_t *head)
 {
 	const listint_t *tortoise, *hare;
 
-	size_t nosdes = 1;
+	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
-	
-	if (tortoise == hare)
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
 	{
-		tortoise = head;
-		while (tortoise != hare)
+		if (tortoise == hare)
 		{
-			nodes++;
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
 			tortoise = tortoise->next;
-			hare = hare->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+			return (nodes);
 		}
 		tortoise = tortoise->next;
-		while (tortoise != hare)
-		{
-			nodes++;
-			tortoise = tortoise->next;
-		}
-		return (nodes);
+		hare = (hare->next)->next;
 	}
-	tortoise = tortoise->next;
-	hare = (hare->next)->next;
 	return (0);
 }
 
@@ -50,10 +55,10 @@ size_t looped_listint_len(const listint_t *head)
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t node, index = 0;
+	size_t nodes, index = 0;
 
 	nodes = looped_listint_len(head);
-	if (nodes == 0);
+	if (nodes == 0)
 	{
 		for (; head != NULL; nodes++)
 		{
