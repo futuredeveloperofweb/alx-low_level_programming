803561e31053de4ec5e22246be5f9e5735d92bbd
othmane <othmane.alaouiekkaoui@gmail.com>
2023-05-08 15:13:13 -0700

    update

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filemane:  is the name of the file to create
+ * @text_content: s a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure (file can not be created, file
+ * can not be written, write “fails”, etc…)
+ * if filename is NULL return -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * append_text_to_file -  appends text at the end of a file.
+ * @filename: is the name of the file
+ * @text_content: is the NULL terminated string to add at the end of the file
+ * Return: 1 on success and -1 on failure, If filename is NULL return -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	
+}
