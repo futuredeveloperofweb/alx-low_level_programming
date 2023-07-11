6562941aabfe7d527346794a7df7ae2b20e778d4
othmane mekkaoui-alaoui <othamne.alaouimekkaoui@gmail.com>
2023-07-11 12:17:48 -0700

    update

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_NOREAD "ERROR: Can't raed from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
+/**
+ * main - the hole code
+ * @ac: arg count
+ * @av: arg vector
+ * Return: 1 on success, 0 otherwize
+ */
+
+int main(int ac, char **av)
+{
+	int from_fd = 0, to_fd = 0;
+	ssize_t b;
+	char buf[READ_BUFFER_SIZE];
+
+	if (ac != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+	from_fd = open(av[1], O_RDONLY);
+	if (from_fd == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (to_fd == -1)
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+	while ((b = read(from_fd, buf, READ_BUFFER_SIZE)) > 0)
+		if (write(to_fd, buf, b) != b)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+
+	if (b == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	from_fd = close(from_fd);
+	if (from_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+	if (to_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+	return (EXIT_SUCCESS);
+}
