9590f2d9bbcb11816ab8fc6f97fb911d55f9d895
othmane <othmane.alaouimekkaoui@gmail.com>
2023-11-02 15:13:48 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,11 +11,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
-	if (!new_table)
+	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!new_table->array)
+	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
