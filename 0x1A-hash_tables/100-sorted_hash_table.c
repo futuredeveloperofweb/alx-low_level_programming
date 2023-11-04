c72ee6ca57c0d78d32272d7b721a8c4703cd25cd
othmane <othmane.alaouimekkaoui@gmail.com>
2023-11-03 18:31:48 -0700

    update

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * shash_table_create - a function that creates a hash table
+ * @size: the size of the hash table
+ * Return: a pointer to the hash table
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+
+}
